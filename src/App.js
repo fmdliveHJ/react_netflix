4b0f5615b1a1950120d46f228b88738d98e64762
hyeonjin <gdalove@naver.com>
2022-10-03 21:23:34 +0900

    이미지 슬라이드 배열 추가 및 푸터 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import requests from './api/requests';
 import Banner from './components/Banner/Banner';
+import Footer from './components/Footer/Footer';
 import Nav from './components/Nav/Nav';
+import Row from './components/Row/Row';
 
 function App() {
 	const path = process.env.PUBLIC_URL;
@@ -8,6 +11,24 @@ function App() {
 		<div className='App'>
 			<Nav path={path} />
 			<Banner path={path} />
+
+			<Row
+				title='nexflix originals'
+				id='NO'
+				fetchUrl={requests.fetchNetflixOriginals}
+				isLargeRow
+			/>
+
+			<Row title='trending now' id='TN' fetchUrl={requests.fetchTrending} />
+			<Row title='top rated' id='TR' fetchUrl={requests.fetchTopRated} />
+			<Row title='action movie' id='TN' fetchUrl={requests.fetchActionMovies} />
+			<Row title='top rated' id='TN' fetchUrl={requests.fetchTrending} />
+			<Row
+				title='comedy movies'
+				id='TN'
+				fetchUrl={requests.fetchComedyMovies}
+			/>
+			<Footer />
 		</div>
 	);
 }
